296780e0250035611bbbb9b6d4ea8656cc078855
Kiruba Nithi <kirubanithi812@gmail.com>
2025-11-19 12:59:08 +0530

    auth

--- GameStore/src/main/java/com/example/GameStore/Repository/UserRepository.java
+++ GameStore/src/main/java/com/example/GameStore/Repository/UserRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-//    Optional<Long> findByUsername(User user);
+    Optional<User> findByUsername(String username);
 }
--- GameStore/src/main/java/com/example/GameStore/Service/AuthService.java
+++ GameStore/src/main/java/com/example/GameStore/Service/AuthService.java
@@ -3,7 +3,9 @@ package com.example.GameStore.Service;
 import com.example.GameStore.Dto.AuthRequest;
 import com.example.GameStore.Entity.User;
 import com.example.GameStore.Repository.UserRepository;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthService {
 
     private  final UserRepository userRepository;
@@ -15,6 +17,9 @@ this.userRepository=userRepository;
 
     public User register(AuthRequest authRequest){
 
+     if (userRepository.findByUsername(authRequest.getUsername()).isPresent()){
+         throw new RuntimeException("Username already taken");
+     }
 
         User user = new User();
         user.setUsername(authRequest.getUsername());
