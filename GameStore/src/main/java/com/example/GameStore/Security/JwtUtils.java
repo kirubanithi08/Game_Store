aac21269972c1afd5d223b786a8e3bb691244027
Kiruba Nithi <kirubanithi812@gmail.com>
2025-11-19 14:04:12 +0530

    jwt

---
+++ GameStore/src/main/java/com/example/GameStore/Security/JwtUtils.java
@@ -0,0 +1,64 @@
+package com.example.GameStore.Security;
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+public class JwtUtils {
+
+    private final Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+
+    private static final long EXPIRATION = 1000 * 60 * 60 * 24;
+
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(key)
+                .compact();
+    }
+
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+
+    public boolean validateToken(String token, String username) {
+        String extractedUsername = extractUsername(token);
+        return username.equals(extractedUsername) && !isTokenExpired(token);
+    }
+
+    private boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+
+}
