18092049af96572a31e2e28bf4c8247ed17b1411
Kiruba Nithi <kirubanithi812@gmail.com>
2025-11-29 16:05:43 +0530

    admin

--- GameStore/src/main/java/com/example/GameStore/Security/CustomUserDetails.java
+++ GameStore/src/main/java/com/example/GameStore/Security/CustomUserDetails.java
@@ -2,6 +2,7 @@ package com.example.GameStore.Security;
 
 import com.example.GameStore.Entity.User;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
@@ -17,7 +18,8 @@ public class CustomUserDetails implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+
+        return List.of(new SimpleGrantedAuthority(user.getRole()));
     }
 
     @Override
