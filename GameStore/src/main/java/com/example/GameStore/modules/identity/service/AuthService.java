efd5638874a672f914c9251e4c5f28baa2077c39
Kirubanithi <118157099+kirubanithi08@users.noreply.github.com>
2026-02-20 10:20:19 +0530

    Change user roles from 'ROLE_USER' to 'USER' and 'ROLE_ADMIN' to 'ADMIN'

--- GameStore/src/main/java/com/example/GameStore/modules/identity/service/AuthService.java
+++ GameStore/src/main/java/com/example/GameStore/modules/identity/service/AuthService.java
@@ -28,7 +28,7 @@ public class AuthService {
         User user = User.builder()
                 .username(authRequest.username())
                 .password(passwordEncoder.encode(authRequest.password()))
-                .role("ROLE_USER")
+                .role("USER")
                 .build();
 
         return userRepository.save(user);
@@ -58,7 +58,7 @@ public class AuthService {
         User admin = User.builder()
                 .username(authRequest.username())
                 .password(passwordEncoder.encode(authRequest.password()))
-                .role("ROLE_ADMIN")
+                .role("ADMIN")
                 .build();
 
         return userRepository.save(admin);
