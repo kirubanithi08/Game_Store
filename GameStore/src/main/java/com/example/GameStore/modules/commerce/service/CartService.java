156754063e6745834862b7030fe8eeede73461d5
Kirubanithi <118157099+kirubanithi08@users.noreply.github.com>
2026-02-21 20:00:37 +0530

    Change user retrieval method from username to email

--- GameStore/src/main/java/com/example/GameStore/modules/commerce/service/CartService.java
+++ GameStore/src/main/java/com/example/GameStore/modules/commerce/service/CartService.java
@@ -23,9 +23,9 @@ public class CartService {
     private final GameRepository gameRepository;
     private final CartRepository cartRepository;
 
-    private User getCurrentUser() {
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        return userRepository.findByUsername(username)
+     private User getCurrentUser() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
     }
 
