131e9e23dcb99126a2b6473376cc656df1863963
Kirubanithi <118157099+kirubanithi08@users.noreply.github.com>
2026-02-21 20:01:11 +0530

    Change getCurrentUser method to use email instead of username

--- GameStore/src/main/java/com/example/GameStore/modules/commerce/service/PurchaseService.java
+++ GameStore/src/main/java/com/example/GameStore/modules/commerce/service/PurchaseService.java
@@ -23,9 +23,9 @@ public class PurchaseService {
     private final GameRepository gameRepository;
     private final PurchaseRepository purchaseRepository;
 
-    private User getCurrentUser() {
-        String username = SecurityContextHolder.getContext().getAuthentication().getName();
-        return userRepository.findByUsername(username)
+     private User getCurrentUser() {
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userRepository.findByEmail(email)
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
     }
 
